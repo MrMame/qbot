68023ef035e36da92973b115a67add67c16b3855
Markus <51000524+MrMame@users.noreply.github.com>
2023-08-09 10:37:51 +0200

    initial commit

---
+++ src/test/java/de/mme/qbot/QbotApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mme.qbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
