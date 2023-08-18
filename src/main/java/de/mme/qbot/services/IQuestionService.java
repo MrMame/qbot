b39ef104423e4ff4cdfc5c69bddb212ab9bbffaf
MrMame <mm@mail.de>
2023-08-18 11:31:39 +0200

    Add UniqueRandomQuestion,getQuestionById and saveQuestion

---
+++ src/main/java/de/mme/qbot/services/IQuestionService.java
@@ -0,0 +1,13 @@
+package de.mme.qbot.services;
+
+import de.mme.qbot.model.domain.Question;
+
+import java.util.Optional;
+
+public interface IQuestionService {
+    public Optional<Question> getUniqueRandomQuestion();
+
+    public Optional<Question> getQuestionById(long id);
+
+    public Question saveQuestion(Question question);
+}
