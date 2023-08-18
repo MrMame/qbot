d5eec44a8eb955a7b6f8864df77910e022b23d2c
MrMame <mm@mail.de>
2023-08-18 11:32:20 +0200

    Add example fill code

--- src/main/java/de/mme/qbot/QbotApplication.java
+++ src/main/java/de/mme/qbot/QbotApplication.java
@@ -1,9 +1,12 @@
 package de.mme.qbot;
 
+import de.mme.qbot.model.domain.Question;
+import de.mme.qbot.repositories.IQuestionRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.origin.SystemEnvironmentOrigin;
@@ -25,6 +28,32 @@ public class QbotApplication {
 
 	}
 
+
+	@Bean
+	public CommandLineRunner demo(IQuestionRepository repository) {
+		return (args) -> {
+			// save a few customers
+			repository.save(new Question("Wieviel uhr haben wir?"));
+			repository.save(new Question("Was hast du gegessen?"));
+			repository.save(new Question("Warum liegt da Heu?"));
+
+			// fetch all customers
+			logger.info("Question found with findAll():");
+			logger.info("-------------------------------");
+			for (Question question : repository.findAll()) {
+				logger.info(question.toString());
+			}
+			logger.info("");
+
+			// fetch an individual customer by ID
+			Question question = repository.findById(1L);
+			logger.info("Question found with findById(1L):");
+			logger.info("--------------------------------");
+			logger.info(question.toString());
+			logger.info("");
+
+		};
+	}
 
 
 }
