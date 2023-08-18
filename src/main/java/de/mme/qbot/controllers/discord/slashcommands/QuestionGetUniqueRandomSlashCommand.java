9e5ab53e12a7cfedf9a16a62a335bde8ad49dcf7
MrMame <mm@mail.de>
2023-08-18 13:07:06 +0200

    Add Command

---
+++ src/main/java/de/mme/qbot/controllers/discord/slashcommands/QuestionGetUniqueRandomSlashCommand.java
@@ -0,0 +1,20 @@
+package de.mme.qbot.controllers.discord.slashcommands;
+
+import de.mme.qbot.controllers.discord.SlashCommandFiredEvent;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+
+import java.util.function.Consumer;
+
+public class QuestionGetUniqueRandomSlashCommand extends AbstractSlashCommand{
+    public final static String COMMAND_NAME ="question-get-unique-random";
+    public final static String COMMAND_DESCRIPTION ="Returns a random question. No duplicates will be returned until all questions were returned";
+
+
+    public QuestionGetUniqueRandomSlashCommand(Consumer<SlashCommandFiredEvent> eventHandler) {
+        CommandData commandData = Commands.slash(COMMAND_NAME,COMMAND_DESCRIPTION);
+
+        this.setCommandData(commandData);
+        this.setCommandHandler(eventHandler);
+    }
+}
