d08f1c90af53214b16f3aea9482269f933fac986
MrMame <mm@mail.de>
2023-08-29 11:02:19 +0200

    #29 - Create Dare table and all set of cruds

--- src/main/java/de/mme/qbot/services/dares/DareRepoService.java
+++ src/main/java/de/mme/qbot/services/dares/DareRepoService.java
@@ -12,7 +12,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-// todo - Original is questionService. Refactor for Dare usage
 
 @Service
 public class DareRepoService implements IDareRepoService {
